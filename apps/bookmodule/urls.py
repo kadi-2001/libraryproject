fd5f976c9e177e139142f2094ea0b97db17dbfca
Kadi-2001 <kadi9062@gmail.com>
2026-03-02 02:43:43 +0300

    lab4

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('index2/text/', views.index2_text),
     path('index2/<int:val1>/', views.index2),
     path('<int:bookId>/', views.viewbook),
+        path('', views.index, name='books.index'),
+    path('list_books/', views.list_books, name='books.list_books'),
+    path('<int:bookId>/', views.viewbook, name='books.view_one_book'),
+    path('aboutus/', views.aboutus, name='books.aboutus'),
 ]
+
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -9,31 +9,29 @@ def index(request):
     return HttpResponse("Hello, " + name)
 
 
-# =========================
-# Task 4 + 5 (Template + Context) ✅
-# =========================
+# Task 4 + 5 
 def home(request):
     name = request.GET.get("name") or "world!"
     return render(request, "bookmodule/index.html", {"name": name})
 
 
-# =========================
-# Task 6 (index2 رقم) ✅
-# =========================
+
+# Task 6 
+
 def index2(request, val1):
     return HttpResponse(f"value1 = {val1}")
 
 
-# =========================
-# Task 6 (index2 نص - Error) ✅
-# =========================
+
+# Task 6 (index2 Error) 
+
 def index2_text(request):
     return HttpResponse("error, expected val1 to be integer")
 
 
-# =========================
-# Task 7 (عرض كتاب) ✅
-# =========================
+
+# Task 7 
+
 def viewbook(request, bookId):
     book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
     book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
@@ -45,3 +43,16 @@ def viewbook(request, bookId):
         targetBook = book2
 
     return render(request, 'bookmodule/show.html', {'book': targetBook})
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'bookmodule/index.html')
+
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
